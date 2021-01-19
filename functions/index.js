4c4247b01b3655e7cc6efd12c6c83a54f773fe2d
Andy <andyjianggg1@gmail.com>
2021-01-19 01:10:05 -0500

    added cors

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require("firebase-functions");
 
 const app = require("express")();
+const cors = require("cors");
+app.use(cors());
 
 const userAuth = require("./util/userAuth");
 
